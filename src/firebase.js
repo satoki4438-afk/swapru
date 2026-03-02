ee621df0a9e77c48bce4d105c3af712c13fc205a
satoki4438-afk <satoki4438@gmail.com>
2026-03-02 18:10:34 +0900

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAVvVXYMT7oaDIXUfQk1of0Jnj-RnAWUHU",
+  authDomain: "swapru.firebaseapp.com",
+  projectId: "swapru",
+  storageBucket: "swapru.firebasestorage.app",
+  messagingSenderId: "310180077020",
+  appId: "1:310180077020:web:8b56e30aeb60e85f9bc319"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const db = getFirestore(app);
